a800a1e7be46f9c903a07e26ac560062b8dac48c
mariamiren <mcarmona8@uoc.edu>
2025-12-09 19:33:03 +0100

    Update prepare_order.py

--- prepare_order.py
+++ prepare_order.py
@@ -8,6 +8,19 @@ products/
         food_package.py
         product.py
 
+
+        yo:
+
+class Product:
+ def __init__(self, name, price):
+    self.name = name
+    self.price = price
+ def info(self):
+    return f"Producto: {self.name},
+Precio: {self.price} Euro"
+
+
+
 El módulo food_package.py contendrá una clase abstracta denominada 'FoodPackage' con dos funciones abstractas: 'def pack(self)  -> str ' y 'def material(self) -> str '. Esta clase nos permite crear un tipo específico de paquete o envoltura dependiendo del tipo de alimento a empacar, por ejemplo:
 
 Un vaso de soda puede ser empacado en un paquete tipo vaso y el material puede ser cartón. 
@@ -20,6 +33,20 @@ class Wrapping(FoodPackage):
     return "Food Wrap Paper"
   def material(self):
     return "Aluminium" 
+
+yo:
+
+from products.soda_cup import SodaCup
+vaso = SodaCup()
+return(vaso.pack() #tipo de vaso
+return(vaso.material() #cartón
+
+from produts.burger_wrap import BurgerWrap
+hamburguesa = BurgerWrap() #envoltura de papel
+return(hamburgesa.pack()) #envolutra de papel
+return(hamburguesa.material()) #Aluminio
+
+ 
 
 El módulo 'product.py’ contendrá una clase abstracta denominada 'Product' con dos funciones abstractas: 'def type(self) -> str' y 'def foodPackage(self)-> FoodPackage. Esta clase nos permita crear un producto específico y relacionarlo con su tipo de empaque por ejemplo:
 
@@ -35,12 +62,111 @@ class Hamburger(Product):
         return "Hamburger"
     def foodPackage(self) -> FoodPackage:
         return Wrapping()
-        
+
+        Yo:
+  
+  from product import Product
+  from soda_cup import SodaCup
+  
+  class Sprite(Product):
+    
+    def __init__(self):
+        self.id = "G1"
+        self.name = "Sprite"
+        self price = 5.0
+
+    def type(self) -> str
+       return "Soda"
+       
+    def goodpackage(self):
+       return SosaCup()
+       
+    def__str__(self):
+       return f"{self.name} {self.code} - {selfprice}
+
+   
+   from burger_warp import BurgerWrap
+
+    class BaconBurger(Product):
+      def __init__(self):
+         self.id = "H1"
+         self.name = "BaconBurger"
+         self price = 15.0
+
+      def type(self) -> str
+        return "Burguer"
+       
+    def goodpackage(self):
+       return BurguerWrap()
+       
+    def__str__(self):
+       return f"{self.name} {self.code} - {selfprice}
+
+   from products.drinks import get_drink_by_code (G1)
+    
+     return(drink.name) # sprite
+     return(drink.goodpackage().pack()) #tipo
+
+from product import Product
+from happymeal import Happymeal
+from soda import Sprite
+from burger_bacon import BurguerBacon
+  
+  class HappyMeal(Product):
+    
+      def __init__(self):
+          self.code = M1
+          self.name = "Happy Meal"
+          self.price = 17.00
+
+          self.drink = Sprite()
+          self.burguer = BaconBurguer()
+      
+      def type(self)-> str
+          return "HappyMeal"
+      
+      def goodpackage(self):
+          return HappyMeal()
+      
+      def cotents(self):
+        return {
+             "burguer": self.burguer
+             "drink": self.drink
+      }
+      def __str__(self):
+          return f" {self name}, {self.code}, {self price}
+      
+       
 Implementar un paquete llamado ‘users' que tiene un módulo ‘user.py', con la siguiente estructura:
 
 users/
         __init__.py
         user.py
+
+yo:
+from users import User
+class User()
+ def__init (self, dni: str, name:str, age: int):
+    self.dni = dni
+    slef.name = name
+    self.age = age
+
+def describe(self) -> str:
+#descripcion del usuario
+
+
+from users import User
+
+class Cashier(User):
+  def __init__(self, dni: str, name: str, age: int)
+  
+  def describe(self) -> str:
+      return f"Cajero{self.name}, {self.age}, {self.age}
+      
+  def process_order(self, order):
+      return(f"{self.name}, {self.order}
+
+        
 
 El módulo 'user.py' contendrá una clase abstracta denominada ‘User’ que tiene un constructor por defecto para los siguientes datos 'def __init__(self, dni:str, name:str, age:int) ', con una función abstracta: 'def describe(self) '.
 
@@ -52,6 +178,55 @@ util/
         __init__.py
         file_manager.py
         converter.py
+
+import pandas as pd
+import os
+
+class CSVFileManager:
+    def __init__(self, folder_path: str):self.folder_path = folder_path
+    
+    def read(self, finlename: str->pd.Dataframe:
+    #lee el archivo CSV y devuelvenData Frame
+         filepath =
+    os.path.join(self.folder_path, filename)
+            if not os path.exists(filepath):
+               raise
+    FileNoFoundError
+            df = pd.read_csv(filepath)
+            return df
+
+    def write(self, df: pd.DataFrame, filename: str):
+    #convierte un dataframe en un archico csv
+           filepath =
+    os.path.join(self.folfer_path, filename)
+           df.to_csv(filepath)
+           return filepath
+
+   from typing import List
+
+     class converter:
+       
+       def convert(self, dataFrame, cls, *arg) -> List:
+          #convierti cada fila de un dataframe en un objto clase, cls.
+           cls= clase destino(customer, cashier, baconburguer, sprite)
+           *arg = atributos en orden de colunas que deben pasarse al constructor
+           
+           objets = []
+           
+           obj = cls(*value)
+           objects.append(obj)
+           return objects
+          
+       def print(self, lista):
+       #imprime informacion dfe la lista de objetos llamando__str__
+     
+         for item in lista
+             print(item)
+
+
+
+
+     
 
 El módulo ‘file_manager.py' contendrá una clase ‘CSVFileManager’ la cual es una implementaciòn libre y debe incluir las funciones:
 
@@ -67,6 +242,7 @@ sodas.csv: Información de los diferentes tipos de gaseosas.
 hamburgers.csv: Información de los diferentes tipos de hamburguesas.
 happyMeal.csv: Información de los diferentes tipos de happy meals.
 
+
 El módulo 'converter.py' contendrá una clase denominada ‘Converter’ con una función abstracta para convertir las filas de un Data Frame en instancias de objetos. La función sería ‘def convert(self, dataFrame, *args) -> list’. Adicionalmente esta clase debe incluir un método que permite imprimir la información de los objetos ‘def print(self, list)’. En el mismo módulo se deberán incluir las implementaciones específicas que permitan leer los archivos en formato CSV y convertir sus filas en objetos de cada clase utilizando los paquetes product y users.
 
 Implementar un paquete llamado 'orders' que tiene un módulo 'order.py', con la siguiente estructura:
@@ -74,6 +250,10 @@ Implementar un paquete llamado 'orders' que tiene un módulo 'order.py', con la 
 orders/
         __init__.py
         order.py
+
+
+
+        
 
 El módulo 'order.py' contendrá una clase denominada ‘Order’ con un constructor ‘def __init__(self, cashier:Cashier, customer:Customer):’, el cual permite inicializar la clase con los datos del cajero, del cliente y la lista de productos vacía por defecto. Además, debe incluir tres funciones para agregar productos, calcular el total de la orden solicitada y mostrar la información de la orden que está siendo procesada. Las funciones son ‘def add(self, product: Product)', ' def calculateTotal(self) -> float' y ‘def show(self)’, respectivamente.
 
